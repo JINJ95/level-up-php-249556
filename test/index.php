25f76723a468fae89c0793aabf98bcd6bf4f60b2
Jordan Johnson <jordaninjohnson@gmail.com>
2023-04-10 20:06:11 +0000

    added birthday

---
+++ test/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Birthday</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <?php 
+        echo '<h1 style="color:blue;text-align:center;">Happy Birthday</h1>';
+        echo '<img style="display: block;margin-left: auto;margin-right: auto;" src="./img/happy-birthday.jpg" alt="Happy Birthday">';
+    ?>
+</body>
+</html>
